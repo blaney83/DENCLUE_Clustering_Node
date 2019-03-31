25c76d5133006c0953c55f22c5a54a19c1ac4d02
Blaney83 <professionallaney@gmail.com>
2019-03-30 21:27:00 -0700

    Testing and debugging.

---
+++ src/io/github/blaney83/dencluecluster/DENCLUEHyperCubeTest.java
@@ -0,0 +1,15 @@
+package io.github.blaney83.dencluecluster;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DENCLUEHyperCubeTest {
+
+	@Test
+	void test() {
+		assertDoubleEquals();
+		
+	}
+
+}
